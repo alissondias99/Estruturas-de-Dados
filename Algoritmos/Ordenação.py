23cd63b468053445619d83d8c5ce229be130ff7d
alissondias99 <dias95357@gmail.com>
2026-05-24 15:20:13 -0300

    Texto sobre ordenação por inserção

--- Algoritmos/Ordenação.py
+++ Algoritmos/Ordenação.py
@@ -1,6 +1,6 @@
 #Ordenação por inserção
 
-lista_a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+lista_a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 10]
 
 n = len(lista_a)
 ord = 1
@@ -14,7 +14,7 @@ for i in range(1, n): # uma laço que ira percorrer a lista começando do índic
     
     j= i - 1 # uma variável que recebe o valor de i -1, começa valenda 0 (porque i começa valendo 1)
     
-    while j>= 0 and lista_a[j]> elem: # enquanto j (que começa valendo 0) for >=0 e o indice j na lista_a for maior que o elemento (lista_a[j] = 9 e elemento =8) 
+    while j>= 0 and lista_a[j] < elem: # enquanto j (que começa valendo 0) for >=0 e o indice j na lista_a for maior que o elemento (lista_a[j] = 9 e elemento =8) 
         
         lista_a[j+1] = lista_a[j]
         '''
@@ -23,7 +23,11 @@ for i in range(1, n): # uma laço que ira percorrer a lista começando do índic
         logo o nove que está no índice 0 passará para o índice 1
         '''
         
-        j = j - 1 # j que ainda está valendo 0 passa a valer -1, isso ocorre para que o loop sejá interrompido e uma nova ordenação possa começar
+        j -= 1 # j que ainda está valendo 0 passa a valer -1, isso ocorre para que o loop sejá interrompido e uma nova ordenação possa começar
     lista_a[j + 1] = elem # atribui ao elemento 0 (j que começou a valer -1 volta a ser 0) o valor contido em elem, que é 8
     print(f"{ord}º ordenação: {lista_a}")
     ord +=1
+    
+    
+# Para fazer em ordem decrescente
+#    while j>= 0 and lista_a[j] < elem: # enquanto j (que começa valendo 0) for >=0 e o indice j na lista_a for MENOR que o elemento (lista_a[j] = 9 e elemento =8) 
